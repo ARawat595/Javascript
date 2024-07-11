bae4228a73251b836518754a86e0ae630069a5da
ARawat595 <162824516+ARawat595@users.noreply.github.com>
2024-07-11 08:17:39 +0000

    save

---
+++ Basic/Variables.js
@@ -0,0 +1,13 @@
+const accountId=12345
+let accountEmail="ankush@gmail.com"
+var accounPassword="12345678"
+accountCity="jaipur"
+
+//accountId=2 //not Allowed as it is declared constant
+
+accountEmail="@agmail.com"
+accountPassword="86745"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accounPassword,accountCity])
