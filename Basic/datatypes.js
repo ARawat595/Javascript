30ab521e3c57b63ed315c83b5b758990b63a764e
ARawat595 <162824516+ARawat595@users.noreply.github.com>
2024-07-11 08:48:58 +0000

    save

---
+++ Basic/datatypes.js
@@ -0,0 +1,19 @@
+"Use Strict"; //treat all Js code as newer version
+
+//alert(3+3) //throw error as we are using nodejs, not browser
+
+
+
+// Prmitive Datatypes Used In javascript
+// 1) number => range from 2 to power 53
+// 2) bigint => used for large numbers 
+// 3) string =>""
+// 4) boolean =>true/false
+// 5) null => Standalone value representation of empty
+// 6) undefined =>value not assigned
+// 7) symbol => unique
+
+
+
+    
+
