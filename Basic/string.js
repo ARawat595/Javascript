a0db7ed679d6e721f0875f4ddd1013b640e5d00e
ARawat595 <162824516+ARawat595@users.noreply.github.com>
2024-07-14 08:03:43 +0000

    savenewchanges

--- Basic/string.js
+++ Basic/string.js
@@ -8,19 +8,19 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 // const gameName = new String('ankush-hc-com')
 const gameName = new String('The morning is upon us.')
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+console.log(gameName[0]);
+console.log(gameName.__proto__);//prints {}
 
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('u'));
+console.log(gameName.charAt(2));//prints e
+console.log(gameName.indexOf('u'));// prints 15
 
-// const newString = gameName.substring(0, 4)
-// console.log(newString);
+const newString = gameName.substring(0, 5)
+console.log(newString);// prints The m
 
-const anotherString = gameName.slice(-8,4);
+const anotherString = gameName.slice(-8,4);//
 console.log(anotherString);
 
 // const newStringOne = "   hitesh    "
@@ -32,7 +32,7 @@ const url = "https://hitesh.com/hitesh%20choudhary"
 
 console.log(url.replace('%20', '-'))// will give https://hitesh.com/hitesh-choudhary
 
-console.log(url.includes('sundar'))//will give true
+console.log(url.includes('sundar'))//will give false
 
 //console.log(gameName.split('-'));// will split in arrays based on - [ 'ankush', 'hc', 'com' ]
-console.log(gameName.split(' '));
+console.log(gameName.split(' '));// prints based on space [ 'The', 'morning', 'is', 'upon', 'us.' ]
