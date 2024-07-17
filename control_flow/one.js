195fb87b222a2cdf76577ee5129f6f008f64144a
ARawat595 <162824516+ARawat595@users.noreply.github.com>
2024-07-17 06:50:21 +0000

    saveone

---
+++ control_flow/one.js
@@ -0,0 +1,58 @@
+//*********If statement******* */
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==, (single = is assignment operator)
+
+//**************************************************************************** */
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);//error accessing power outside scope
+
+//**************************************************************************** */
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2");//short hand notation of writing if() is called implict scope it execute in one line only 
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+//**************************************************************************** */
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
